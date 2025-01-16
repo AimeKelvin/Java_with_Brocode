567be7e29f06f03363c9e51b36e2b4ca6839a59d
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-01-16 16:48:13 +0200

    learning java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-
+        int num = 1;
         Scanner scanner = new Scanner(System.in);
         scanner.close();
 
